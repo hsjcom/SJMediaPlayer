a25bb4338b325a84d09070d8f735dabf42a7adae
Shaojie Hong <hongsjcn@qq.com>
2015-09-15 14:50:23 +0800

    Initial Commit

---
+++ SJMediaPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJMediaPlayer
+//
+//  Created by Soldier on 15/9/15.
+//  Copyright (c) 2015年 Shaojie Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJMediaPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJMediaPlayer
+//
+//  Created by Soldier on 15/9/15.
+//  Copyright (c) 2015年 Shaojie Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
